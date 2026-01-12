501cdb428b3e184c7d69159b5ad9f8b2fc6177a2
Jai Srivastava <jai.srivastava@iphtechnologies.com>
2026-01-12 13:26:31 +0530

    Refactor: Fix Work screen to use shared Task model and prevent crashes

--- DayList/WorkTask/Controller/WorkViewController.swift
+++ DayList/WorkTask/Controller/WorkViewController.swift
@@ -1,29 +1,96 @@
-//
-//  WorkViewController.swift
-//  DayList
-//
-//  Created by iPHTech4 on 1/12/26.
-//
-
 import UIKit
 
 class WorkViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var badgeLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var headerContainerView: UIView!
+
+    // MARK: - Data
+    var tasks: [Task] = []
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupTableView()
+        loadTasks()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        badgeLabel.layer.cornerRadius = 6
+        badgeLabel.layer.masksToBounds = true
     }
-    */
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
 
+// MARK: - UI Setup
+extension WorkViewController {
+
+    private func setupUI() {
+        titleLabel.text = "Work"
+        
+        if #available(iOS 13.0, *) {
+            backButton.setImage(UIImage(systemName: "line.horizontal.3"), for: .normal)
+        } else {
+            backButton.setTitle("Menu", for: .normal)
+        }
+        backButton.tintColor = .black
+    }
+
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 80
+        
+        // REMOVED: setupAddTaskHeader()
+        // We removed this line so it won't create the duplicate button
+    }
+}
+
+// MARK: - DataSource & Delegate
+extension WorkViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as! TaskTableViewCell
+        let task = tasks[indexPath.row]
+        cell.configure(with: task)
+        return cell
+    }
+}
+
+// MARK: - Data
+extension WorkViewController {
+    func loadTasks() {
+        tasks = [
+            Task(title: "Research content ideas"),
+            Task(title: "Create a database of guest authors"),
+            Task(title: "Create job posting for SEO specialist"),
+            Task(title: "Request design assets for landing page",
+                 dateText: "22-03-22",
+                 subtaskCountText: "2",
+                 listName: "Work",
+                 tagColor: .systemTeal),
+            Task(title: "Prepare collaboration proposals"),
+            Task(title: "Adopt a link tracker tool")
+        ]
+        
+        badgeLabel.text = "\(tasks.count)"
+        tableView.reloadData()
+    }
 }
