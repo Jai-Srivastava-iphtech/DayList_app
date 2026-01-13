fafe1f7458d8b8f2440bb7bdd4e7a8013184b9a0
Jai Srivastava <jai.srivastava@iphtechnologies.com>
2026-01-13 18:27:40 +0530

    Add WeekEvent data model

--- DayList/Calendar/Model/WeekEvent.swift
+++ DayList/Calendar/Model/WeekEvent.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+struct WeekEvent {
+    let title: String
+    let color: UIColor
+}
+
+private var weekEvents: [[WeekEvent]] = [
+    [ WeekEvent(title: "Se..", color: .systemTeal),
+      WeekEvent(title: "Sa..", color: .systemTeal) ],
+
+    [ WeekEvent(title: "Co..", color: .systemYellow) ],
+
+    [ WeekEvent(title: "Br..", color: .systemTeal) ],
+
+    [ WeekEvent(title: "Bu..", color: .systemYellow) ],
+
+    [], [], []
+]
